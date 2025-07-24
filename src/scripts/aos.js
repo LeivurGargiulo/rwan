eaa8253f680e29e7cbaf2eaad12720725c5b0288
LeivurGargiulo <leivurgargiulo@gmail.com>
2025-07-24 17:05:36 -0300

    disabled AOS on mobiles

--- src/scripts/aos.js
+++ src/scripts/aos.js
@@ -1,5 +1,6 @@
 import AOS from "aos";
 AOS.init({
+  disable: "mobile", // disable on mobile devices
   duration: 800, // animation duration
   once: false, // animate again on scroll up
   offset: 100, // trigger position
